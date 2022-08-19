f8ce408fcf0b7e39e74b0cad1425f9337b586a65
DiGiDaNRE <57100357+DiGiDaNRE@users.noreply.github.com>
2022-08-19 08:35:48 +0800

    Update GlobalSQA_locators.py

--- locators/GlobalSQA_locators.py
+++ locators/GlobalSQA_locators.py
@@ -1,5 +1,3 @@
-from testdata.GlobalSQA_testdata import CustomerName
-from testdata.GlobalSQA_testdata import AccountNumber
 
 #HomePage
 CustomerButton = "//*[contains(text(),'Customer Login')]"
@@ -10,7 +8,7 @@ HomePageButton = "//*[contains(text(),'Home')]"
 #CustomerPage
 CustomerPageVerify = "//*[contains(text(),'Your Name :')]"
 CustomerDropDownActiate = "//*[@id='userSelect']"
-SelectName = "//*[contains(text(),'"+str(CustomerName)+"')]"
+SelectName = "//*[contains(text(),'Harry Potter')]"
 LoginButton = "//*[contains(text(),'Login')]"
 
 #CustomerAccount
@@ -18,7 +16,7 @@ Cus_TransactionButton = "//*[contains(text(),'Transactions')]"
 Cus_DepositButton = "//*[@ng-click='deposit()' and contains(text(),'Deposit')]"
 Cus_Withdrawl = "//*[contains(text(),'Withdrawl')]"
 Cus_AccountNoDropDown = "//*[@id='accountSelect']"
-SelectAccountNo = "//*[@label='"+str(AccountNumber)+"']"
+SelectAccountNo = "//*[@label='1004']"
 AmountInput = "//*[@placeholder='amount']"
 Cus_ConfirmDepositButton = "//*[@type='submit' and contains(text(),'Deposit')]"
 
